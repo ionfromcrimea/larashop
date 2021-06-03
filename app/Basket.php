8d7086a8670c70d0a0e8e0cab5a4ee039f422b8a
Oleg <ionfromcrimea@gmail.com>
2021-06-03 13:31:15 +0300

    5. Создаем корзину покупателя, добавление товара в корзину

---
+++ app/Basket.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Basket extends Model {
+    /**
+     * Связь «многие ко многим» таблицы `baskets` с таблицей `products`
+     */
+    public function products() {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+    }
+}
