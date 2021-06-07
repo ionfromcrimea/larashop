15e9856918262b874977ae4828ea91d23b2f80a3
Oleg <ionfromcrimea@gmail.com>
2021-06-07 13:56:05 +0300

    7. Меню каталога товаров и популярные бренды в левой колонке Кнопки "плюс-минус" прорисованы библиотекой glyphicon

--- app/Category.php
+++ app/Category.php
@@ -17,14 +17,14 @@ class Category extends Model {
      * Возвращает список корневых категорий каталога товаров
      */
     public static function roots() {
-        return self::where('parent_id', 0)->get();
+        return self::where('parent_id', 0)->with('children')->get();
     }
 
     /**
      * Связь «один ко многим» таблицы `categories` с таблицей `categories`
      */
     public function children() {
-        $q = $this->hasMany(Category::class, 'parent_id');
+//        $q = $this->hasMany(Category::class, 'parent_id');
 //        dd($q->modelKeys());
         return $this->hasMany(Category::class, 'parent_id');
     }
