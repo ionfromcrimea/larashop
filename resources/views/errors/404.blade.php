452d98b3588f82108dc330ceabcba1774e11be6b
Oleg <ionfromcrimea@gmail.com>
2021-07-26 13:20:44 +0300

    Сообщение на странице 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,14 +5,15 @@
         <div class="col-12">
             <div class="card mt-4 mb-4">
                 <div class="card-header">
-                    <h3>Страница не найдена</h3>
+                    <h4>Страница не найдена</h4>
                 </div>
                 <div class="card-body">
                     <img src="{{ asset('img/png404-2.png') }}" alt="" class="img-fluid">
 {{--                    <img src="{{ asset('storage/catalog/category/image/4nQNOmMr6WHVZjsbIlgxrUnNPJpevNgTIfUjJeDM.jpg ') }}" alt="" class="img-fluid">--}}
                 </div>
                 <div class="card-footer">
-                    <p>Запрошенная страница не найдена.</p>
+{{--                    <p>Запрошенная страница не найдена.</p>--}}
+                    <h3>{{ isset($exception) ? $exception->getMessage() : "" }}</h3>
                 </div>
             </div>
         </div>
